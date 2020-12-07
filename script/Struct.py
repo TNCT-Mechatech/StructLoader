595c1c89cdec54e925dd0efd73787eb5b308c820
owner <forgotten_saga_1129@outlook.jp>
2020-12-07 20:16:16 +0900

    [Update] remove unnecessary variables

--- script/Struct.py
+++ script/Struct.py
@@ -1,22 +1,17 @@
 #encoding=utf-8
-# File: StructClass
+# File: Struct
 # Discription: struct class(struct_name,msg_id(int),List<Pair<variable_name,variable_type>>) 
 # Date: 2020/12/7
 # Author: testusuke
 # GitHub: https://github.com/testusuke
 
-# StructClass
 # This class includes struct_name,msg_id(Int),variable_list<variable<variable_name,variable_type>>
 # struct_name[string]
 # msg_id[Int]
 # variable_list[List]
 # variable_name[String]
-# variable_type[String] <-- ???????
-class StructClass:
-    struct_name = ""
-    msg_id = 0
-    head_index = 0
-
+# variable_type[String]
+class Struct:
     def __init__(self,struct_name,msg_id):
         self.struct_name = str(struct_name)
         self.msg_id = int(msg_id)
