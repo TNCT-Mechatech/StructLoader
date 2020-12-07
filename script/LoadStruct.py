359ce19b2c7776d40074bb7ce5be5926fcae34e4
owner <forgotten_saga_1129@outlook.jp>
2020-12-07 20:18:00 +0900

    [Update] add function of load struct

--- script/LoadStruct.py
+++ script/LoadStruct.py
@@ -1,15 +1,39 @@
 #encoding=utf-8
-# File: LoadStructClass
+# File: LoadStruct
 # Discription: you are able to get struct class(struct_name,msg_id(int),List<Pair<variable_name,variable_type>>) 
 # Date: 2020/12/6
 # Author: testusuke
 # GitHub: https://github.com/testusuke
 
-class LoadStructClass:
-    path = ""
+import Struct
+import os
+import traceback
 
+class LoadStruct:
     def __init__(self,path):
-        print("init load struct list ")
+        print("info: init LoadStruct")
         self.path = path
-        sc = StructClass("",1)
-        return sc
+        # load struct
+        self.load_struct()
+
+        return self.struct
+        
+    def load_struct(self):
+        print("info: loading file:" + self.path)
+        # check exist file
+        if not os.path.exists(self.path):
+            try:
+                raise ValueError("Error: fine not exist")
+            except ValueError as e:
+                traceback.print_exc()
+        # check file type
+        root, ext = os.path.splitext(self.path)
+        if ext != '.yml':
+            try:
+                raise ValueError("Error: file is not yaml file")
+            except ValueError as e:
+                traceback.print_exc()
+        
+        # create instance
+        self.struct = Struct(name,msg_id)
+        # add variables
